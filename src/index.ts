02fe47f05bf084fad3de2baeb9a15b1d8bdacf29
s0558040 <s0558040@htw-berlin.de>
2020-12-20 16:59:42 +0100

    Configured testing framework

--- src/index.ts
+++ src/index.ts
@@ -22,13 +22,13 @@ app.use('/feedback', feedbackRouter);
 app.use('/cirs_feedback', expressJwt({ secret: TOKEN_SECRET, algorithms: ['HS256'] }), safeFeedbackRouter);
 app.use('/classification', classificationRouter);
 app.use('/cirs_classification', expressJwt({ secret: TOKEN_SECRET, algorithms: ['HS256'] }), safeClassificationRouter);
-/* app.use((error, request, response, next) => {
+app.use((error, request, response, next) => {
     if (error.name === 'UnauthorizedError') {
         response.status(401).json('unauthorized');
     } else {
         next();
     }
-}); */
+});
 
 app.listen(8080, () => {
     console.log('Server is listening to http://localhost:8080');
